b5235da3a9d628c21095ec6ddab65f0f566a8097
Lulia131313 <146219985+Lulia131313@users.noreply.github.com>
2024-04-05 22:16:06 +0300

    get

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ContactForm from "./componrnts/contactForm/ContactForm";
+import s from "../src/componrnts/";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="">Phonebook</h1>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default App;
